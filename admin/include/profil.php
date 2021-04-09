3f442169dabe3ddfe5e6f242af4080e8ef887cfc
Novandre <82073107+Novandre@users.noreply.github.com>
2021-04-09 12:18:00 +0700

    menambahkan folder admin dan backend pada profil

--- admin/include/profil.php
+++ admin/include/profil.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-<?php include("includes/head.php") ?> 
-</head>
-<body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<?php include("includes/header.php") ?>
+<?php
+$id_user = $_SESSION['id_user'];
+//get profil
+$sql = "select `nama`, `email`, `foto` from `user` where `id_user`='$id_user'";
+//echo $sql;
+$query = mysqli_query($koneksi, $sql);
+while($data = mysqli_fetch_row($query)){
+	$nama = $data[0];
+	$email = $data[1];
+	$foto = $data[2];
+}
+?>
 
-  <?php include("includes/sidebar.php") ?>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
@@ -32,13 +31,17 @@
             <div class="card">
               <div class="card-header">
                 <div class="card-tools">
-                  <a href="editprofil.php" class="btn btn-sm btn-info float-right"><i class="fas fa-edit"></i> Edit Profil</a>
+                  <a href="index.php?include=edit-profil" class="btn btn-sm btn-info float-right"><i class="fas fa-edit"></i> Edit Profil</a>
                 </div>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
                 <div class="col-sm-12">
-                    <div class="alert alert-success" role="alert">Data Berhasil Diubah</div>
+                    <?php if(!empty($_GET['notif'])){
+						if($_GET['notif']=="editberhasil"){?>
+						<div class="alert alert-success" role="alert">Data Berhasil Diubah</div>
+						<?php }?>
+					<?php }?>
                 </div>
                 <table class="table table-bordered">
                     <tbody>  
@@ -47,15 +50,15 @@
                       </tr> 
                       <tr>
                         <td width="20%"><strong>Foto<strong></td>
-                        <td width="80%"><img src="foto/salnan.jpg" class="img-fluid" width="200px;"></td>
+                        <td width="80%"><img src="foto/<?php echo $foto;?>" class="img-fluid" width="200px;"></td>
                       </tr>                
                       <tr>
                         <td width="20%"><strong>Nama<strong></td>
-                        <td width="80%">Salnan Ratih A.</td>
+                        <td width="80%"><?php echo $nama; ?></td>
                       </tr>                
                       <tr>
                         <td width="20%"><strong>Email<strong></td>
-                        <td width="80%">salnanratih@gmail.com</td>
+                        <td width="80%"><?php echo $email;?></td>
                       </tr> 
                     </tbody>
                   </table>  
@@ -67,7 +70,7 @@
 
     </section>
     <!-- /.content -->
-  </div>
+	</div>
   <!-- /.content-wrapper -->
   <?php include("includes/footer.php") ?>
 
@@ -75,5 +78,3 @@
 <!-- ./wrapper -->
 
 <?php include("includes/script.php") ?>
-</body>
-</html>
