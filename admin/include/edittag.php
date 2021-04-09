2803bc10cfe59fee358ec96e0de4c5cdbbf04da2
Novandre <82073107+Novandre@users.noreply.github.com>
2021-04-09 12:16:48 +0700

    menambahkan folder admin dan backend pada menu tag

--- admin/include/edittag.php
+++ admin/include/edittag.php
@@ -1,16 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-<?php include("includes/head.php") ?> 
-</head>
-<body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<?php include("includes/header.php") ?>
 
-  <?php include("includes/sidebar.php") ?>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<?php 
+if(isset($_GET['data'])){
+	$id_tag = $_GET['data'];
+	$_SESSION['id_tag']=$id_tag;
+	
+	//get data tag
+	$sql_a = "select `tag` from `tag` where `id_tag` = '$id_tag'";
+	$query_a = mysqli_query($koneksi,$sql_a);
+	while($data_a = mysqli_fetch_row($query_a)){
+		$tag = $data_a[0];
+	}
+}
+?>
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
@@ -20,7 +21,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="katgoribuku.php">Tag</a></li>
+              <li class="breadcrumb-item"><a href="index.php?include=tag">Tag</a></li>
               <li class="breadcrumb-item active">Edit Tag</li>
             </ol>
           </div>
@@ -35,21 +36,25 @@
       <div class="card-header">
         <h3 class="card-title"style="margin-top:5px;"><i class="far fa-list-alt"></i> Form Edit Tag</h3>
         <div class="card-tools">
-          <a href="tag.php" class="btn btn-sm btn-warning float-right"><i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
+          <a href="index.php?include=tag" class="btn btn-sm btn-warning float-right"><i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
         </div>
       </div>
       <!-- /.card-header -->
       <!-- form start -->
       </br>
       <div class="col-sm-10">
-          <div class="alert alert-danger" role="alert">Maaf data Tag wajib di isi</div>
+		<?php if(!empty($_GET['notif'])){?>
+			<?php if($_GET['notif']=="editkosong"){?>
+			<div class="alert alert-danger" role="alert">Maaf data Tag wajib di isi</div>
+			<?php }?>
+		<?php }?>
       </div>
-      <form class="form-horizontal">
+      <form class="form-horizontal" method="post" action="index.php?include=konfirmasi-edit-tag">
         <div class="card-body">
           <div class="form-group row">
             <label for="Tag" class="col-sm-3 col-form-label">Tag</label>
             <div class="col-sm-7">
-              <input type="text" class="form-control" id="Tag" value="PHP">
+              <input type="text" class="form-control" id="tag" Name="tag" value="<?php echo $tag;?>">
             </div>
           </div>
         </div>
@@ -74,5 +79,3 @@
 <!-- ./wrapper -->
 
 <?php include("includes/script.php") ?>
-</body>
-</html>
