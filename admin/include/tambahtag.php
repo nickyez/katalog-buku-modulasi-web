b79314631d0457d2b0937761f811a3f5ddb17e9c
Novandre <82073107+Novandre@users.noreply.github.com>
2021-04-09 12:15:52 +0700

    menambahkan folder admin dan backend pada menu tag

--- admin/include/tambahtag.php
+++ admin/include/tambahtag.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<?php include("includes/head.php") ?> 
-</head>
-<body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<?php include("includes/header.php") ?>
-
-  <?php include("includes/sidebar.php") ?>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
@@ -20,7 +7,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="tag.php">Tag</a></li>
+              <li class="breadcrumb-item"><a href="index.php?include=tag">Tag</a></li>
               <li class="breadcrumb-item active">Tambah Tag</li>
             </ol>
           </div>
@@ -35,21 +22,25 @@
       <div class="card-header">
         <h3 class="card-title"style="margin-top:5px;"><i class="far fa-list-alt"></i> Form Tambah Tag</h3>
         <div class="card-tools">
-          <a href="tag.php" class="btn btn-sm btn-warning float-right"><i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
+          <a href="index.php?include=tag" class="btn btn-sm btn-warning float-right"><i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
         </div>
       </div>
       <!-- /.card-header -->
       <!-- form start -->
       </br>
       <div class="col-sm-10">
-          <div class="alert alert-danger" role="alert">Maaf data tag wajib di isi</div>
+			<?php if(!empty($_GET['notif'])){?>
+			<?php if($_GET['notif']=="tambahkosong"){?>
+				<div class="alert alert-danger" role="alert">Maaf data tag wajib di isi</div>
+				<?php }?>
+			<?php }?>
       </div>
-      <form class="form-horizontal">
+      <form class="form-horizontal" method="post" action="index.php?include=konfirmasi-tambah-tag">
         <div class="card-body">
           <div class="form-group row">
             <label for="tag" class="col-sm-3 col-form-label">Tag</label>
             <div class="col-sm-7">
-              <input type="text" class="form-control" id="tag" value="">
+              <input type="text" class="form-control" id="tag" name="tag" value="">
             </div>
           </div>
         </div>
@@ -74,5 +65,3 @@
 <!-- ./wrapper -->
 
 <?php include("includes/script.php") ?>
-</body>
-</html>
